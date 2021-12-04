77b8e07ac5eb74d4cf0b5251433c4b220df38031
Roman <47324027+cyber-roNO@users.noreply.github.com>
2021-12-04 22:59:21 +0300

    added signUp style

--- mobile/components/auth/SignUpForm.js
+++ mobile/components/auth/SignUpForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TouchableOpacity, View, Text } from 'react-native';
+import { TouchableOpacity, View } from 'react-native';
 import { Formik, Field } from 'formik';
 import PropTypes from 'prop-types';
 
@@ -7,7 +7,7 @@ import { TextInputField } from '../shared/FormFields';
 import { signUpValidationRules } from '../../validation/auth';
 import $t from 'i18n';
 import ErrorText from '../shared/Text/ErrorText';
-import { Input } from 'react-native-magnus';
+import { Input,Text , Checkbox,Div} from 'react-native-magnus';
 
 export const SignUpForm = ({ onSubmit, signUpErrors }) => (
   <Formik
@@ -24,33 +24,26 @@ export const SignUpForm = ({ onSubmit, signUpErrors }) => (
     {({ handleSubmit }) => (
       <View>
         <TouchableOpacity onPress={handleSubmit}>
-          <Text fontSize="4xl" textAlign="center" mb="xl" >{$t('auth.signUp')}</Text>
+          <Text fontSize="4xl" textAlign="center" mb="xl">{$t('auth.signUp')}</Text>
         </TouchableOpacity>
-
-        <Field
-          name="first_name"
-          component={TextInputField}
-          placeholder={$t('auth.enterFirstName')}
-        />
-
-        <Field name="last_name" placeholder={$t('auth.enterLastName')}>
-          {({ field, form }) => <Input {...form} placeholder="Фамилия" {...inputStyle} />}
+   
+        <Field name="first_name" 
+        >
+          {({ field, form }) => <Input {...form} placeholder={$t('auth.enterFirstName')} {...inputStyle} />}
         </Field>
-        
-        <Field name="email" component={TextInputField} placeholder={$t('auth.enterEmail')} />
-        <ErrorText error={!!signUpErrors.email} message={signUpErrors.email} />
-        <Field
-          name="password"
-          component={TextInputField}
-          secureTextEntry
-          placeholder={$t('auth.enterPassword')}
-        />
-        <Field
-          name="confirm_password"
-          component={TextInputField}
-          secureTextEntry
-          placeholder={$t('auth.confirmPassword')}
-        />
+        <Field name="last_name">
+          {({ field, form }) => <Input {...form}  placeholder={$t('auth.enterLastName')} {...inputStyle} />}
+        </Field>
+        <Field name="email" >
+          {({ field, form }) => <Input {...form}  placeholder={$t('auth.enterEmail')} {...inputStyle} />}
+        </Field>
+        <Field name="password" >
+          {({ field, form }) => <Input {...form} secureTextEntry placeholder={$t('auth.enterPassword')} {...inputStyle} />}
+        </Field>
+      
+        <Checkbox flexDir="row-reverse" value={1} prefix={<Text textAlign="right">Согласен с правилами сервиса</Text>} />
+
+    
       </View>
     )}
   </Formik>
@@ -77,4 +70,8 @@ export const buttonSubmitStyle = {
   bg: "#000",
   w:169,
   alignSelf: 'center'
+}
+
+export const textStyle = {
+  fontSize: 14,
 }
--- mobile/screens/auth/SignUpScreen.js
+++ mobile/screens/auth/SignUpScreen.js
@@ -3,10 +3,12 @@ import { StyleSheet, View } from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import $t from 'i18n';
 import { useDispatch, useSelector } from 'react-redux';
+import { Button, Text } from 'react-native-magnus';
 
 import { SignUpForm } from '../../components/auth/SignUpForm';
 import { signUp } from '../../store/auth';
 import { setSignUpErrors, signUpErrorsSelector } from '../../store/error';
+import { buttonSubmitStyle } from '../../components/auth/SignUpForm';
 
 const SignUpScreen = () => {
   const dispatch = useDispatch();
@@ -24,6 +26,9 @@ const SignUpScreen = () => {
     <View style={styles.container}>
       <KeyboardAwareScrollView enableOnAndroid>
         <SignUpForm onSubmit={handleSignUp} signUpErrors={signUpErrors} />
+        <Button {...buttonSubmitStyle}>
+          Отправить
+        </Button>
       </KeyboardAwareScrollView>
     </View>
   );
@@ -37,6 +42,7 @@ export default SignUpScreen;
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#fff',
-    flex: 1
+    flex: 1,
+    marginTop: 60
   }
 });
